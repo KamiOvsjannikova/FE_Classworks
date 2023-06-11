f95512ad701672266a8fbe983a4ea12edad1cf25
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-11 19:05:18 +0300

    array_js

---
+++ project8_js/script.js
@@ -0,0 +1,75 @@
+/*alert("hello")
+console.log("hello")
+const name = prompt('Kak tebja zovut')
+console.log(name) */
+// alert("hello") // alert - это браузерное уведомление
+// console.log("hello") // отображение в консоли
+// const name = prompt("Как тебя зовут?") // функция для ввода информации
+// console.log(name) 
+
+// const userAge = 40
+// const userLogin = "Starta Institute"
+// console.log(userAge) // значение переменной userAge - то что хранит переменная
+// console.log("userAge") // строка "userAge"
+
+/*
+    1. строка - "Arsen", "Hello", "My title", "2023", "500", "995557447807"
+    2. число - 100, 500, 2023, 700, 1000, 10.5, 3.14
+*/
+
+/*
+    Задачи. 
+        1. Создать переменные для хранения названия продукта, имени пользователя, цены продукта 
+        2. Отобразить каждую переменную в консоли
+*/
+/*const productName = "Milk"
+const userName = "Kamilla"
+const productPrice = 10000
+
+console.log(productName, productPrice)
+console.log(userName)
+*/
+
+
+/* 
+let price = 2000
+const price1 = 3000
+
+price = 5000
+price1 = 7000
+console.log(price)
+console.log(price1)
+*/
+
+/* const number = +prompt("Vvedite pervoe chislo ")
+const number1 = +prompt("Vvedite vtoroe chislo")
+console.log(number + number1)
+*/
+
+/*const number = +prompt("Vvedite pervoe chislo ")
+console.log(number ** 3)
+console.log(Math.pow(number, 2))*/
+
+
+/*const name = prompt("Vvedite vawe imja")
+console.log("Hallo, " + name)
+console.log(`Hallo, ${name}`)
+*/
+
+// Задача. Запросить у пользователя имя и возраст. В консоли вывести 
+// "Привет, {введенное имя}. Вам {введенный возраст} лет"
+/*const name = prompt("Enter Name")
+const age = prompt("Enter Age")
+console.log(`Hallo, ${name}. Vam ${age} let`)
+console.log("Привет, " + username + ". Вам " + userage + " лет")
+*/
+
+
+/* const r = ("15")
+const g = ("51")
+const b = ("120")
+console.log(`rgb (${r}, ${g}, ${b})`)
+*/
+
+
+
