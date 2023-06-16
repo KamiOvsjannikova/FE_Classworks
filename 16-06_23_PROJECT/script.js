e4d295aed6b1369d8e887af0d003941f9ec7190d
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2023-06-16 14:00:10 +0300

    mini project

---
+++ 16-06_23_PROJECT/script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector('textarea');
+const counter = document.querySelector('#counter');
+
+text.addEventListener('input', function () {
+   counter.textContent = text.value.length;  
+});
